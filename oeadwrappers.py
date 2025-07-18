fb9dc15e961042fd9ed1711e66add223f1194fb5
techmuse <44119098+techmuse8@users.noreply.github.com>
2025-07-18 15:59:31 -0400

    code refactoring
    I split my oead wrapper functions into their own file for a cleaner codebase for when I eventually implement the GUI
    
    Also added a stub for randomizing dialogue

---
+++ oeadwrappers.py
@@ -0,0 +1,76 @@
+import oead
+import os
+from pathlib import Path
+
+# A set of wrapper functions around oead for handling SARC and BYAML files for Splatoon 1
+
+def extractSARC(inputSARC):
+    with open(inputSARC, "rb") as f:
+        sarcData = f.read()
+
+    if oead.yaz0.get_header(sarcData):
+        sarcData = oead.yaz0.decompress(sarcData)
+
+    sarc = oead.Sarc(sarcData)
+
+    outputDir = Path(f"{inputSARC}_extracted")
+    outputDir.mkdir(exist_ok=True)
+
+    for file in sarc.get_files():
+        outputPath = outputDir / file.name
+        parentDir = outputPath.parent
+
+        if parentDir.exists() and not parentDir.is_dir():
+            print(f"Removing conflicting file: {parentDir}")
+            parentDir.unlink()
+
+        parentDir.mkdir(parents=True, exist_ok=True)
+
+        with open(outputPath, "wb") as out_file:
+            out_file.write(file.data)
+
+def packSARC(inputDir: str, outputPath: str, compress: bool = True):
+    inputPath = Path(inputDir)
+
+    builder = oead.SarcWriter(endian=oead.Endianness.Big, mode=oead.SarcWriter.Mode.Legacy)
+
+    for filePath in inputPath.rglob("*"):
+        if filePath.is_file():
+            relativePath = filePath.relative_to(inputPath).as_posix()
+            with open(filePath, "rb") as f:
+                builder.files[relativePath] = f.read()
+
+    sarcData = builder.write()
+    
+    if isinstance(sarcData, tuple):
+       sarcData = sarcData[1]
+
+    if compress:
+        sarcData = oead.yaz0.compress(sarcData, 0, 7)
+
+    with open(outputPath, "wb") as f:
+        f.write(sarcData)
+
+    print(f"Packed SARC: {outputPath}")
+    
+def convertFromBYAML(inputBYAML):
+    # print(inputBYAML)
+    with open(inputBYAML, "rb") as f:
+        info = oead.byml.from_binary(f.read())
+        ouputYAML = oead.byml.to_text(info)
+        # print(ouputYAML)
+        outputYamlName = os.path.splitext(inputBYAML)
+        print(outputYamlName[0])
+    with open(f"{outputYamlName[0]}.yaml", "w") as file:
+        file.write(ouputYAML)
+        
+def convertToBYAML(inputYaml):
+    with open(inputYaml, "r", encoding="utf-8") as file:
+        content = file.read()
+     
+    convdBYAML = oead.byml.from_text(content)
+    outputBYAMLName = os.path.splitext(inputYaml)
+    print(outputBYAMLName[0])
+    
+    with open(f"{outputBYAMLName[0]}.byaml", "wb") as f:
+        f.write(oead.byml.to_binary(data=convdBYAML, big_endian=True, version=1))
--- randomizer.py
+++ randomizer.py
@@ -1,81 +1,12 @@
 import os
-import oead
-from ruamel.yaml import YAML
 import random
 from pathlib import Path
+import shutil
 
-def extractSARC(inputSARC):
-    with open(inputSARC, "rb") as f:
-        sarcData = f.read()
-
-    if oead.yaz0.get_header(sarcData):
-        sarcData = oead.yaz0.decompress(sarcData)
-
-    sarc = oead.Sarc(sarcData)
-
-    outputDir = Path(f"{inputSARC}_extracted")
-    outputDir.mkdir(exist_ok=True)
-
-    for file in sarc.get_files():
-        outputPath = outputDir / file.name
-        parentDir = outputPath.parent
-
-        if parentDir.exists() and not parentDir.is_dir():
-            print(f"Removing conflicting file: {parentDir}")
-            parentDir.unlink()
-
-        parentDir.mkdir(parents=True, exist_ok=True)
-
-        with open(outputPath, "wb") as out_file:
-            out_file.write(file.data)
-
-def packSARC(inputDir: str, outputPath: str, compress: bool = True):
-    inputPath = Path(inputDir)
-
-    builder = oead.SarcWriter(endian=oead.Endianness.Big, mode=oead.SarcWriter.Mode.Legacy)
-
-    for filePath in inputPath.rglob("*"):
-        if filePath.is_file():
-            relativePath = filePath.relative_to(inputPath).as_posix()
-            with open(filePath, "rb") as f:
-                builder.files[relativePath] = f.read()
-
-    sarcData = builder.write()
-    
-    if isinstance(sarcData, tuple):
-       sarcData = sarcData[1]
-
-    if compress:
-        sarcData = oead.yaz0.compress(sarcData, 0, 7)
-
-    with open(outputPath, "wb") as f:
-        f.write(sarcData)
-
-    print(f"Packed SARC: {outputPath}")
-    
-def convertFromBYAML(inputBYAML):
-    # print(inputBYAML)
-    with open(inputBYAML, "rb") as f:
-        info = oead.byml.from_binary(f.read())
-        ouputYAML = oead.byml.to_text(info)
-        # print(ouputYAML)
-        outputYamlName = os.path.splitext(inputBYAML)
-        print(outputYamlName[0])
-    with open(f"{outputYamlName[0]}.yaml", "w") as file:
-        file.write(ouputYAML)
-        
-def convertToBYAML(inputYaml):
-    with open(inputYaml, "r", encoding="utf-8") as file:
-        content = file.read()
-     
-    convdBYAML = oead.byml.from_text(content)
-    outputBYAMLName = os.path.splitext(inputYaml)
-    print(outputBYAMLName[0])
-    
-    with open(f"{outputBYAMLName[0]}.byaml", "wb") as f:
-        f.write(oead.byml.to_binary(data=convdBYAML, big_endian=True, version=1))
-
-
+from oeadwrappers import *
+from ruamel.yaml import YAML
+from LMS.Message.MSBTStream import read_msbt as readMSBT
+from LMS.Message.MSBTStream import write_msbt as writeMSBT
 
 extractSARC('Static.pack')
 World00ArchivePath = 'Static.pack_extracted/Map/Fld_World00_Wld.szs'
@@ -186,7 +117,10 @@ def updateStageNumbers(): # Updates the MapInfo yaml with the correct stage numb
 
     with open('Static.pack_extracted/Mush/MapInfo.yaml', 'w') as f:
         f.writelines(updatedStageNo)
-        
+
+def randomizeDialogue():
+    pass
+
 def updateBossStageNumbers(): # Updates the MapInfo yaml with the correct boss stage numbers, this is seperate due to the different numbering pattern for bosses
                               # TODO: somehow merge this with updateStageNumber?
     with open('Static.pack_extracted/Mush/MapInfo.yaml', 'r') as f:
