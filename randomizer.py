a430f38d6941280d68d4a0117d60ba41fcde8ddc
techmuse <44119098+techmuse8@users.noreply.github.com>
2025-08-09 19:36:53 -0400

    os.rmdir -> shutil.rmtree

--- randomizer.py
+++ randomizer.py
@@ -317,7 +317,7 @@ def rebuildStaticPack(extractedStaticPackDir):
                     print(f"Error deleting {filePath}: {e}")
     if isKettles:
         packSARC(f"{World00ArchivePath}_extracted", World00ArchivePath, compress=True)
-    os.rmdir(f"{World00ArchivePath}_extracted") # Cleanup
+    shutil.rmtree(f"{World00ArchivePath}_extracted") # Cleanup
     packSARC(extractedStaticPackDir, packDirectoryPath + 'Static.pack', compress=False)
 
 def addEditedWeaponUpgradeUI():
@@ -408,4 +408,5 @@ def setupRandomization(splatoonFilesystemRoot, randomizerSeed, options):
         rebuildStaticPack(packDirectoryPath + 'Static.pack_extracted')
 
     performFinishingTouches(options, splatoonFilesystemRoot)
+
 
