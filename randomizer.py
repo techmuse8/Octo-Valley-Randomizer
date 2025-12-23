2a9139ff0d14dd2388ca3d00feecd6ca3a116509
techmuse <44119098+techmuse8@users.noreply.github.com>
2025-12-23 10:04:12 -0500

    Majorish randomizer.py refactor
    This is to make updating the randomizer more easier for myself.
    
    - Switch to having a randomizer context object that gets passed around over global spam
    - Use Path objects instead of manually concatting the strings in most areas
    - Fixed a bug in performFinishingTouches where the custom text wouldn't always get added

--- randomizer.py
+++ randomizer.py
@@ -5,6 +5,7 @@ import shutil
 import logging
 import time
 import io
+from dataclasses import dataclass
 
 from oeadwrappers import *
 from ruamel.yaml import YAML
@@ -17,14 +18,25 @@ from lms.project.msbp import MSBP
 
 
 
-packDirectoryPath = ''
+#str(ctx.packDirPat) = ''
 stageNames = []
 bossStageNames = []
 startLine = 5
 
-def randomizeKettles():
+@dataclass
+class RandomizerContext:
+    root: Path
+    packDirPath: Path
+    staticPackDir: Path
+    layoutDir: Path
+    mapInfoYAML: Path | None
+    world00ArchivePath: Path | None = None
+    isKettles: bool = False
+
+
+def randomizeKettles(ctx: RandomizerContext):
     startLine = 5
-    with open(f"{World00ArchivePath}_extracted/Fld_World00_Wld.yaml") as f:
+    with open(f"{str(ctx.world00ArchivePath)}_extracted/Fld_World00_Wld.yaml") as f:
         lines = f.readlines()[startLine:]
         stageNames = [ # Loop through every word in the YAML and filter out all of the stage names
             word for line in lines
@@ -45,12 +57,12 @@ def randomizeKettles():
     stageNames.append('Fld_BossRailKing_Bos_Msn') # Make sure the final boss stage isn't randomized
     bossStageNames.append('Fld_BossRailKing_Bos_Msn')
 
-    addRandomizedKettles(f"{World00ArchivePath}_extracted/Fld_World00_Wld.yaml", stageNames)
+    addRandomizedKettles(f"{str(ctx.world00ArchivePath)}_extracted/Fld_World00_Wld.yaml", stageNames)
     print(bossStageNames)
     print(stageNames)
-    updateStageNumbers(mapInfoYAML, stageNames)
-    updateBossStageNumbers(mapInfoYAML, bossStageNames)
-    updateStageIcons(ogFullStageNames, stageNames)
+    updateStageNumbers(ctx.mapInfoYAML, stageNames)
+    updateBossStageNumbers(ctx.mapInfoYAML, bossStageNames)
+    updateStageIcons(ctx, ogFullStageNames, stageNames)
 
 def addRandomizedKettles(filePath, replacementStageNames):
     yaml = YAML()
@@ -90,14 +102,14 @@ def randomizeMusic(mapInfoYAML):
     with open(mapInfoYAML, 'w') as f:
         yaml.dump(data, f)
 
-def randomizeInkColors(mapInfoYAML, inkColorSetting):
+def randomizeInkColors(ctx: RandomizerContext, inkColorSetting):
     yaml = YAML()
     yaml.preserve_quotes = True
 
     # List of possible ink colors
     inkColors =  ['DarkBlue', 'Green', 'Lilac', 'LumiGreen', 'NightLumiGreen', 'MothGreen', 'Marigold', 'NightMarigold', 'Orange', 'Soda', 'Turquoise', 'Yellow']
 
-    with open(mapInfoYAML, 'r') as f:
+    with open(ctx.mapInfoYAML, 'r') as f:
         data = yaml.load(f)
 
     if not isinstance(data, list):
@@ -107,13 +119,13 @@ def randomizeInkColors(mapInfoYAML, inkColorSetting):
         if 'TeamColor_Msn' in obj:
             obj['TeamColor_Msn'] = random.choice(inkColors)
     if inkColorSetting == 1 or 2:
-        addVSInkColors(inkColorSetting)
-    with open(mapInfoYAML, 'w') as f:
+        addVSInkColors(ctx, inkColorSetting)
+    with open(ctx.mapInfoYAML, 'w') as f:
         yaml.dump(data, f)
 
-def addVSInkColors(setting):
-    parameterPath = staticPackDir + os.path.join("Parameter")
-    workFolder = (os.path.join(parameterPath, "work"))
+def addVSInkColors(ctx: RandomizerContext, setting):
+    parameterPath = ctx.staticPackDir / "Parameter"
+    workFolder = parameterPath / "work"
     os.makedirs(workFolder, exist_ok=True)
     vsInkColors = []
     msnInkColors = []
@@ -153,7 +165,7 @@ def delete4RandomInkColors(folderPath, count=4):
     for file in filesToDelete:
         fullPath = os.path.join(folderPath, file)
         os.remove(fullPath)
-        print(f"Deleted: {file}")
+      #  print(f"Deleted: {file}")
 
 
 def updateStageNumbers(mapInfoYAMLPath, stageNames): # Updates the MapInfo yaml with the correct stage numbers so collecting Zapfish will work correctly
@@ -380,21 +392,26 @@ def updateBossStageNumbers(mapInfoYAML, bossStageNames): # Updates the MapInfo y
     with open(mapInfoYAML, 'w') as f:
         f.writelines(updatedBossStageNo)
         
-def updateStageIcons(originalStageOrder, shuffledStageOrder):
+def updateStageIcons(ctx: RandomizerContext, originalStageOrder, shuffledStageOrder):
     """
     Updates the stage icons to match the current randomized stages.
     """
-    stageIconLayoutContainer = packDirectoryPath + 'Layout.pack_extracted/Layout/MsnStageIcon_00.szs'
-    stageIconLayoutArchive = packDirectoryPath + 'Layout.pack_extracted/Layout/MsnStageIcon_00.szs_extracted/MsnStageIcon_00.arc' # The archive layering here is annoying but we deal with it
+    stageIconLayoutContainer = ctx.packDirPath / 'Layout.pack_extracted' / 'Layout' / 'MsnStageIcon_00.szs'
+    stageIconLayoutArchive = stageIconLayoutContainer.with_name(stageIconLayoutContainer.stem + '.szs_extracted') / 'MsnStageIcon_00.arc' # The archive layering here is annoying but we deal with it
 
-    extractSARC(packDirectoryPath + 'Layout.pack')
+    extractSARC(ctx.packDirPath / 'Layout.pack')
     extractSARC(stageIconLayoutContainer)
     extractSARC(stageIconLayoutArchive)
-    stageIconDir = packDirectoryPath + 'Layout.pack_extracted/Layout/MsnStageIcon_00.szs_extracted/MsnStageIcon_00.arc_extracted/timg'
+
+    stageIconLayoutArchiveExtracted = stageIconLayoutArchive.with_name(stageIconLayoutArchive.name + '_extracted')
+    stageIconDir = stageIconLayoutArchiveExtracted / 'timg'
+
     original_index_map = {name: str(index).zfill(2) for index, name in enumerate(originalStageOrder)}
     # Maps the original stage numbers to the new one
     oldToNewStageMapping = {name: shuffledStageOrder.index(name) for name in originalStageOrder}
-    os.makedirs(stageIconDir + '/remapped_images')
+    remappedDir = stageIconDir / 'remapped_images'
+    remappedDir.mkdir(exist_ok=True)
+
 
     print("Generated mapping:")
     for old_stage, new_index in oldToNewStageMapping.items():
@@ -409,40 +426,47 @@ def updateStageIcons(originalStageOrder, shuffledStageOrder):
             if newStageNumber is not None:
                 newFilename = filename.replace(f'_{ogStageNumber}^q', f'_{str(newStageNumber).zfill(2)}^q')
             
-                oldFilePath = os.path.join(stageIconDir, filename)
-                newFilePath = os.path.join(stageIconDir + '/remapped_images', newFilename)
-                os.rename(oldFilePath, newFilePath)
+                oldFilePath = stageIconDir / filename
+                newFilePath = remappedDir / newFilename
+                oldFilePath.rename(newFilePath)
+
                 print(f'Renamed: {filename} -> {newFilename}')
             else:
                 print(f"WARNING: No new stage number found for {ogStageName}")
     
-    for filename in os.listdir(stageIconDir + '/remapped_images'):
-        print(filename)
-        shutil.move(stageIconDir + '/remapped_images/' + filename, stageIconDir)
-    
-    packLayoutArchives('MsnStageIcon_00')
+    for filePath in remappedDir.iterdir():
+        shutil.move(str(filePath), str(stageIconDir))
 
-def extractLayoutArchives(layoutFilename):
+    
+    packLayoutArchives(ctx, 'MsnStageIcon_00')
+
+def extractLayoutArchives(ctx: RandomizerContext, layoutFilename):
     """A function to make unpacking layout archives less tedious."""
-    layoutContainer = (os.path.join(layoutFolder, layoutFilename + '.szs'))
+    layoutContainer = (os.path.join(str(ctx.layoutDir), layoutFilename + '.szs'))
     layoutArchive = layoutContainer + f'_extracted/{layoutFilename}.arc'
 
     extractSARC(layoutContainer)
     extractSARC(layoutArchive)
 
-def packLayoutArchives(layoutFilename):
+def packLayoutArchives(ctx: RandomizerContext, layoutFilename):
     """A function to make packing layout archives less tedious."""
-    layoutContainer = (os.path.join(layoutFolder, layoutFilename + '.szs'))
-    layoutArchive = layoutContainer + f'_extracted/{layoutFilename}.arc'
+    layoutContainer = ctx.layoutDir / f"{layoutFilename}.szs"
+    layoutContainerExtracted = layoutContainer.with_name(layoutContainer.stem + '.szs_extracted')
 
-    packSARC(layoutArchive + '_extracted', layoutArchive, compress=False)
-    shutil.rmtree(layoutArchive + '_extracted')
-    packSARC(layoutContainer + '_extracted', layoutContainer, compress=True)
-    shutil.rmtree(layoutContainer + '_extracted')
+    layoutArchive = layoutContainer.with_name(layoutContainer.stem + '.szs_extracted') / f"{layoutFilename}.arc"
+    layoutArchiveExtracted = layoutArchive.with_name(layoutArchive.stem + '.arc_extracted')
+
+    packSARC(layoutArchiveExtracted, layoutArchive, compress=False)
+    shutil.rmtree(layoutArchiveExtracted)
+
+    packSARC(layoutContainerExtracted, layoutContainer, compress=True)
+    shutil.rmtree(layoutContainerExtracted)
 
 
-def rebuildStaticPack(extractedStaticPackDir):
-    for dirpath, dirnames, filenames in os.walk(extractedStaticPackDir):
+def rebuildStaticPack(ctx: RandomizerContext):
+    staticPackPath = ctx.packDirPath / 'Static.pack'
+    
+    for dirpath, dirnames, filenames in os.walk(ctx.staticPackDir):
         for filename in filenames:
             if filename.endswith('.yaml'):
                 filePath = os.path.join(dirpath, filename)
@@ -451,39 +475,40 @@ def rebuildStaticPack(extractedStaticPackDir):
                     print(f"Deleted: {filePath}")
                 except OSError as e:
                     print(f"Error deleting {filePath}: {e}")
-    if isKettles:
-        packSARC(f"{World00ArchivePath}_extracted", World00ArchivePath, compress=True)
-        shutil.rmtree(f"{World00ArchivePath}_extracted") # Cleanup
-    packSARC(extractedStaticPackDir, packDirectoryPath + 'Static.pack', compress=False)
+    if ctx.isKettles:
+        packSARC(f"{str(ctx.world00ArchivePath)}_extracted", str(ctx.world00ArchivePath), compress=True)
+        shutil.rmtree(f"{str(ctx.world00ArchivePath)}_extracted") # Cleanup
+    time.sleep(1.5)
+    packSARC(ctx.staticPackDir, staticPackPath, compress=False)
 
-def addEditedWeaponUpgradeUI():
+def addEditedWeaponUpgradeUI(ctx: RandomizerContext):
     """Adds in the edited weapon upgrade UI with a footnote nothing that only the Hero Shot can be upgraded."""
-    extractLayoutArchives('Wdm_Reinforce_00')
-    extWeaponUpgradeLayoutDir = layoutFolder + 'Wdm_Reinforce_00.szs_extracted/Wdm_Reinforce_00.arc_extracted/'
+    extractLayoutArchives(ctx, 'Wdm_Reinforce_00')
+    extWeaponUpgradeLayoutDir = ctx.layoutDir / 'Wdm_Reinforce_00.szs_extracted' / 'Wdm_Reinforce_00.arc_extracted'
 
-    shutil.copy('assets/Weapon Upgrade UI/Wdm_Reinforce_00.bflyt', extWeaponUpgradeLayoutDir + 'blyt/Wdm_Reinforce_00.bflyt')
-    packLayoutArchives('Wdm_Reinforce_00')
+    shutil.copy('assets/Weapon Upgrade UI/Wdm_Reinforce_00.bflyt', extWeaponUpgradeLayoutDir / 'blyt' / 'Wdm_Reinforce_00.bflyt')
+    packLayoutArchives(ctx, 'Wdm_Reinforce_00')
 
-def addLayoutEdits(options):
+def addLayoutEdits(ctx: RandomizerContext, options):
     """Adds in the randomizer logo, the custom tutorial image and text."""
-    extractLayoutArchives('Tut_TutorialPicture_00')
-    extractLayoutArchives('Plz_Title_00')
+    extractLayoutArchives(ctx, 'Tut_TutorialPicture_00')
+    extractLayoutArchives(ctx, 'Plz_Title_00')
 
-    extTutorialLayoutArchiveDir = layoutFolder + 'Tut_TutorialPicture_00.szs_extracted/Tut_TutorialPicture_00.arc_extracted/'
-    extTitleLayoutArchiveDir = layoutFolder + 'Plz_Title_00.szs_extracted/Plz_Title_00.arc_extracted/'
+    extTutorialLayoutArchiveDir = ctx.layoutDir / 'Tut_TutorialPicture_00.szs_extracted' / 'Tut_TutorialPicture_00.arc_extracted'
+    extTitleLayoutArchiveDir = ctx.layoutDir / 'Plz_Title_00.szs_extracted' / 'Plz_Title_00.arc_extracted'
 
-    shutil.copy('assets/Rando Title Screen UI and Logo/GambitLogo_00^l.bflim', extTitleLayoutArchiveDir + 'timg/GambitLogo_00^l.bflim')
-    shutil.copy('assets/Rando Title Screen UI and Logo/Plz_Title_00.bflyt', extTitleLayoutArchiveDir + 'blyt/Plz_Title_00.bflyt')
-    shutil.copy('assets/Tutorial Images and Text/TutorialPic_00^o.bflim', extTutorialLayoutArchiveDir + 'timg/TutorialPic_00^o.bflim')
-    shutil.copy('assets/Tutorial Images and Text/TutorialPic_01^o.bflim', extTutorialLayoutArchiveDir + 'timg/TutorialPic_01^o.bflim')
+    shutil.copy('assets/Rando Title Screen UI and Logo/GambitLogo_00^l.bflim', extTitleLayoutArchiveDir / 'timg' / 'GambitLogo_00^l.bflim')
+    shutil.copy('assets/Rando Title Screen UI and Logo/Plz_Title_00.bflyt', extTitleLayoutArchiveDir / 'blyt' / 'Plz_Title_00.bflyt')
+    shutil.copy('assets/Tutorial Images and Text/TutorialPic_00^o.bflim', extTutorialLayoutArchiveDir / 'timg' / 'TutorialPic_00^o.bflim')
+    shutil.copy('assets/Tutorial Images and Text/TutorialPic_01^o.bflim', extTutorialLayoutArchiveDir / 'timg' / 'TutorialPic_01^o.bflim')
 
-    packLayoutArchives('Tut_TutorialPicture_00')
-    packLayoutArchives('Plz_Title_00')
+    packLayoutArchives(ctx, 'Tut_TutorialPicture_00')
+    packLayoutArchives(ctx, 'Plz_Title_00')
 
     if options["heroWeapons"]:
-        addEditedWeaponUpgradeUI()
+        addEditedWeaponUpgradeUI(ctx)
 
-    packSARC(packDirectoryPath + 'Layout.pack_extracted', packDirectoryPath + 'Layout.pack', compress=False)
+    packSARC(ctx.packDirPath / 'Layout.pack_extracted', ctx.packDirPath / 'Layout.pack', compress=False)
 
 def addCustomText(splatoonRandoFiles):
     for item in os.listdir(f"{splatoonRandoFiles}/Message"):
@@ -492,59 +517,63 @@ def addCustomText(splatoonRandoFiles):
             shutil.copy("assets/Tutorial Images and Text/Narration_Tutorial.msbt", f"{splatoonRandoFiles}/Message/" + item + '_extracted/Narration/Narration_Tutorial.msbt')
             packSARC(f"{splatoonRandoFiles}/Message/" + item + '_extracted', f"{splatoonRandoFiles}/Message/" + item, compress=True)
 
-def performFinishingTouches(options, splatoonFilesystemRoot):
-    if os.path.isdir(packDirectoryPath + 'Layout.pack_extracted'):
-        addLayoutEdits(options)
+def performFinishingTouches(ctx: RandomizerContext, options, splatoonFilesystemRoot):
+    if os.path.isdir(ctx.packDirPath / 'Layout.pack_extracted'):
+        addLayoutEdits(ctx, options)
+        addCustomText(splatoonFilesystemRoot)
     else:
-        extractSARC(packDirectoryPath + 'Layout.pack')
-        addLayoutEdits(options)
+        extractSARC(ctx.packDirPath / 'Layout.pack')
+        addLayoutEdits(ctx, options)
         addCustomText(splatoonFilesystemRoot)
     
 def setupRandomization(splatoonFilesystemRoot, randomizerSeed, options):
     random.seed(randomizerSeed)
-    global World00ArchivePath, packDirectoryPath, mapInfoYAML, updatedStageNo, stageNames, staticPackDir, isKettles, layoutFolder
-    updatedStageNo = []
-    packDirectoryPath = splatoonFilesystemRoot + '/Pack/'
-    staticPackDir = packDirectoryPath + 'Static.pack_extracted/'
-    layoutFolder = packDirectoryPath + 'Layout.pack_extracted/Layout/'
-    isKettles = False
-    mapFolderPath = splatoonFilesystemRoot + '/Pack/' + 'Static.pack_extracted/Map/'
+
+    ctx = RandomizerContext(
+    root=Path(splatoonFilesystemRoot),
+    packDirPath=Path(splatoonFilesystemRoot) / "Pack",
+    staticPackDir=Path(splatoonFilesystemRoot) / "Pack/Static.pack_extracted",
+    layoutDir=Path(splatoonFilesystemRoot) / "Pack/Layout.pack_extracted/Layout",
+    mapInfoYAML=None
+)
+
+    mapFolderPath = ctx.root / 'Pack' / 'Static.pack_extracted' / 'Map'
 
     if options["kettles"] or options["inkColors"] or options["music"] or options["heroWeapons"] or options["enemies"]:
-        extractSARC(splatoonFilesystemRoot + '/Pack/' + 'Static.pack')
-        convertFromBYAML(packDirectoryPath + 'Static.pack_extracted/Mush/MapInfo.byaml')
-        mapInfoYAML = packDirectoryPath + 'Static.pack_extracted/Mush/MapInfo.yaml'
+        extractSARC(str(ctx.packDirPath / 'Static.pack'))
+        convertFromBYAML(str(ctx.staticPackDir / 'Mush/MapInfo.byaml'))
+        ctx.mapInfoYAML = ctx.staticPackDir / 'Mush/MapInfo.yaml'
         
     if options["kettles"]:
         print("Randomizing Kettles")
-        isKettles = True
-        World00ArchivePath = splatoonFilesystemRoot + '/Pack/' + 'Static.pack_extracted/Map/Fld_World00_Wld.szs'
+        ctx.isKettles = True
+        ctx.world00ArchivePath = Path(splatoonFilesystemRoot) / 'Pack' / 'Static.pack_extracted/Map/Fld_World00_Wld.szs'
         
-        extractSARC(World00ArchivePath)
-        convertFromBYAML(World00ArchivePath + '_extracted/Fld_World00_Wld.byaml')
-        randomizeKettles()
-        convertToBYAML(f"{World00ArchivePath}_extracted/Fld_World00_Wld.yaml")
+        extractSARC(ctx.world00ArchivePath)
+        convertFromBYAML(Path(f"{ctx.world00ArchivePath}_extracted") / "Fld_World00_Wld.byaml")
+        randomizeKettles(ctx)
+        convertToBYAML(f"{str(ctx.world00ArchivePath)}_extracted/Fld_World00_Wld.yaml")
 
     if options["music"]:
         print("Randomizing Music")
-        randomizeMusic(mapInfoYAML)
+        randomizeMusic(ctx.mapInfoYAML)
 
     if options["inkColors"]:   
         print("Randomizing ink colors")
-        randomizeInkColors(mapInfoYAML, options["inkColorSet"])
+        randomizeInkColors(ctx, options["inkColorSet"])
 
     if options["missionDialogue"]:     
         print("Randomizing Dialogue")
-        randomizeDialogue(splatoonFilesystemRoot)
+        randomizeDialogue(str(ctx.root))
     
     if options["enemies"]:
         randomizeEnemies(mapFolderPath)
 
     if options["kettles"] or options["inkColors"] or options["music"] or options["heroWeapons"] or options["enemies"]:
-        convertToBYAML(mapInfoYAML)
-        rebuildStaticPack(packDirectoryPath + 'Static.pack_extracted')
+        convertToBYAML(ctx.mapInfoYAML)
+        rebuildStaticPack(ctx)
 
     
 
-    performFinishingTouches(options, splatoonFilesystemRoot)
+    performFinishingTouches(ctx, options, str(ctx.root))
     return True
